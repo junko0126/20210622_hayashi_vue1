94c6fab0bfff55254f24ca97e3543e96521c42b3
junko0126 <hikari2011h.j@gmail.com>
2021-06-22 11:02:17 +0900

    first commit

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,81 @@
+<template>
+  <footer class="top-footer">
+          <div class="footer-bar">
+            <div class="flex">
+              <h1 class="header-logo">estra inc.</h1>
+              <nav class="header-nav">
+                <ul>
+                  <li><a href="#">home</a></li>
+                  <li><a href="#">about</a></li>
+                  <li><a href="#">service</a></li>
+                  <li><a href="#">works</a></li>
+                  <li><a href="#">price</a></li>
+                  <li><a href="#">contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+        </footer>
+</template>
+
+<style>
+.top-footer {
+  background: #000;
+  text-align: center;
+}
+
+.top-footer small {
+  text-align: center;
+  color: #fff;
+}
+
+.footer-bar {
+  height: 70px;
+  line-height: 70px;
+}
+
+@media screen and (max-width: 768px) {
+  .top-footer {
+    margin: 0;
+    width: 100%;
+    text-align: center;
+  }
+  .menu-content {
+    display: block;
+    width: calc(100% - 80px);
+    height: 100%;
+    text-align: center;
+    transition: 0.2s;
+    position: fixed;
+    top: 0;
+    left: calc(-100% - 80px);
+    background: #fff;
+    color: #333333;
+    box-shadow: 80px 0 rgba(38, 98, 213, 0.3);
+    z-index: 999;
+  }
+  .menu-content .menu-title {
+    margin: 40px 20px;
+    font-size: 2.2rem;
+    color: #333333;
+    text-align: left;
+    font-weight: bold;
+  }
+  .menu-content li a {
+    color: #333333;
+    margin: 15px;
+    padding: 5px;
+    border-bottom: 0.5px solid #2662d5;
+    text-decoration: none;
+    display: block;
+  }
+  .menu-content li {
+    width: 100%;
+    text-align: left;
+  }
+  .menu-content.open {
+    left: 0;
+  }
+}
+</style>
